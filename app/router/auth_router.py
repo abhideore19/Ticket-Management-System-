329d89309d0805bb8b5dbe7966f74bf9d0acc7f7
abhideore19 <abhideore19@gmail.com>
2026-06-17 12:01:39 +0530

    Update auth_router.py

--- app/router/auth_router.py
+++ app/router/auth_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from app.database import get_db
-from app.models import User
-from app.schemas import UserRegister, UserLogin
-from app.utils.security import (
+from database import get_db
+from models import User
+from schemas import UserRegister, UserLogin
+from utils.security import (
     hash_password,
     verify_password
 )
